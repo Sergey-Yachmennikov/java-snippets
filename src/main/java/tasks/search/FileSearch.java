1c39fa77d1fe54b8d5d22024e4f609a3bb7bd9e0
Сергей Ячменников <sergejacmennikov@Sergejs-MacBook-Pro.local>
2024-08-05 16:55:59 +0300

    added FileSearch

---
+++ src/main/java/tasks/search/FileSearch.java
@@ -0,0 +1,24 @@
+package tasks.search;
+
+import java.io.File;
+import java.util.List;
+
+public class FileSearch {
+
+    public static void searchFiles(File root, List<File> fileList) {
+        if (root.isDirectory()) {
+            File[] directoryFiles = root.listFiles();
+            if (directoryFiles != null) {
+                for (File file : directoryFiles) {
+                    if (file.isDirectory()) {
+                        searchFiles(file, fileList);
+                    } else {
+                        if (file.getName().toLowerCase().endsWith(".jpg")) {
+                            fileList.add(file);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
