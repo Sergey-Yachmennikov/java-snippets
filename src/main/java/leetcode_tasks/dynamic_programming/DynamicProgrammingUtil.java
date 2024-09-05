2dc36d02f9c7a15bc6e912d6447e334ada295bcd
Sergey-Yachmennikov <otsmmm2013@gmail.com>
2024-09-05 20:16:24 +0300

    added wordBreak algorithm

--- src/main/java/leetcode_tasks/dynamic_programming/DynamicProgrammingUtil.java
+++ src/main/java/leetcode_tasks/dynamic_programming/DynamicProgrammingUtil.java
@@ -1,22 +1,42 @@
 package leetcode_tasks.dynamic_programming;
 
+import java.util.List;
+
 public class DynamicProgrammingUtil {
 
     private DynamicProgrammingUtil() {}
 
     public static int climbStairs(int n) {
 
-        if(n == 0 || n == 1) {
-            return 1;
-        }
+        if(n <= 1) return 1;
 
-        int dp[] = new int[n+1];
+        int[] dp = new int[n + 1];
         dp[0] = dp[1] = 1;
 
         for(int i = 2; i <= n; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
 
         return dp[n];
+    }
+
+    // tabulation approach
+    public static boolean wordBreak(String s, List<String> wordDict) {
+        boolean[] dp = new boolean[s.length()];
+        for (int i  = 0 ; i < s.length(); i++) {
+            for (String word : wordDict) {
+                if (i < word.length() - 1) continue;
+                int start = i - word.length();
+
+                if (i == word.length() - 1 || dp[start]) {
+                    if (s.substring(start + 1, i + 1).equals(word)) {
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return dp[s.length() - 1];
     }
 }
--- src/test/java/leetcode_tasks/dynamic_programming/DynamicProgrammingUtilTest.java
+++ src/test/java/leetcode_tasks/dynamic_programming/DynamicProgrammingUtilTest.java
@@ -2,6 +2,8 @@ package leetcode_tasks.dynamic_programming;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DynamicProgrammingUtilTest {
@@ -14,5 +16,12 @@ class DynamicProgrammingUtilTest {
         assertEquals(3, DynamicProgrammingUtil.climbStairs(3));
         assertEquals(5, DynamicProgrammingUtil.climbStairs(4));
         assertEquals(89, DynamicProgrammingUtil.climbStairs(10));
+    }
+
+    @Test
+    void wordBreak() {
+        assertTrue(DynamicProgrammingUtil.wordBreak("leetcode", List.of("leet","code")));
+        assertTrue(DynamicProgrammingUtil.wordBreak("applepenapple", List.of("apple","pen")));
+        assertFalse(DynamicProgrammingUtil.wordBreak("catsandog", List.of("cats","dog","sand","and","cat")));
     }
 }
