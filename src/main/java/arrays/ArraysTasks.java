b9fa87858fb5fe95c542d1a0d0a02adc8737719c
Sergey-Yachmennikov <otsmmm2013@gmail.com>
2024-09-07 17:16:28 +0300

    added containerWithMostWater algorithm

--- src/main/java/arrays/ArraysTasks.java
+++ src/main/java/arrays/ArraysTasks.java
@@ -5,6 +5,9 @@ public class ArraysTasks {
 
     private ArraysTasks() {}
 
+    /**
+     * @topic Bitwise
+     */
     public static int findUniqueItem(int[] array) {
         int rest = array[0];
 
@@ -13,5 +16,29 @@ public class ArraysTasks {
         }
 
         return rest;
+    }
+
+    /**
+     * @topic Two pointers
+     */
+    public static int containerWithMostWater(int[] lines) {
+        int result = Integer.MIN_VALUE;
+        int leftLine = 0;
+        int rightLine = lines.length - 1;
+
+        while (leftLine < rightLine) {
+            int width = rightLine - leftLine;
+            int height = Math.min(lines[leftLine], lines[rightLine]);
+            int area = width * height;
+            result = Math.max(result, area);
+
+            if (lines[leftLine] > lines[rightLine]){
+                rightLine--;
+            } else {
+                leftLine++;
+            }
+        }
+
+        return result;
     }
 }
--- src/test/java/arrays/ArraysTasksTest.java
+++ src/test/java/arrays/ArraysTasksTest.java
@@ -11,4 +11,9 @@ class ArraysTasksTest {
         int[] arr = new int[]{1,1,3,4,4};
         assertEquals(3, ArraysTasks.findUniqueItem(arr));
     }
+
+    @Test
+    void containerWithMostWater() {
+        assertEquals(49, ArraysTasks.containerWithMostWater(new int[]{1,8,6,2,5,4,8,3,7}));
+    }
 }
